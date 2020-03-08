2606cc8818264cdcf1ac28093850ca3e122b391e
David Zlobinskyy <bureauzlobi@gmail.com>
2020-03-08 11:43:03 +0100

    add site url to site-meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: `https://davidzlobinskyy.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
