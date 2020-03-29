6cac2e247477d297e5bf7c0307772a493adaa335
David Zlobinskyy <bureauzlobi@gmail.com>
2020-03-29 16:59:22 +0200

    Remove history related components and code

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,14 +4,9 @@ import { Footer } from './footer';
 import { GlobalStyle, darkTheme, lightTheme } from 'styles';
 import { ThemeProvider } from 'styled-components';
 import { ThemeSwitch } from './themeSwitch';
-import { LocationProvider, createHistory } from '@reach/router';
 
 export const Layout: React.FC<{}> = ({ children }) => {
   const [appTheme, setAppTheme] = useState('light');
-
-  let history = createHistory(window);
-
-  // for some types of tests you want a memory source
 
   useLayoutEffect(() => {
     const localTheme = localStorage.getItem('theme');
@@ -22,21 +17,19 @@ export const Layout: React.FC<{}> = ({ children }) => {
   }, []);
 
   return (
-    <LocationProvider history={history}>
-      <ThemeProvider theme={appTheme === 'light' ? lightTheme : darkTheme}>
-        <GlobalStyle />
-        <Header />
-        <main style={{ padding: '50px 0 100px 0' }}>{children}</main>
-        <ThemeSwitch
-          theme={appTheme}
-          onClick={() => {
-            const theme = appTheme === 'light' ? 'dark' : 'light';
-            setAppTheme(theme);
-            localStorage.setItem('theme', theme);
-          }}
-        />
-        <Footer appTheme={appTheme} />
-      </ThemeProvider>
-    </LocationProvider>
+    <ThemeProvider theme={appTheme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Header />
+      <main style={{ padding: '50px 0 100px 0' }}>{children}</main>
+      <ThemeSwitch
+        theme={appTheme}
+        onClick={() => {
+          const theme = appTheme === 'light' ? 'dark' : 'light';
+          setAppTheme(theme);
+          localStorage.setItem('theme', theme);
+        }}
+      />
+      <Footer appTheme={appTheme} />
+    </ThemeProvider>
   );
 };
